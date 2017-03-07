823cf851ab814142c985fb2b485cb8297759ced4
shubham <shubham0075@gmail.com>
2017-03-07 19:02:32 +0530

    tested dynamic redirect method

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-   return redirect("http://sh.ubham.com",code='302')
+   return redirect("http://sh.ubham.com",code=302)
+
+@app.route('/<page>/')
+def redirect_function(page):
+    if page == 'hi':
+        return redirect("http://github.com",code=302)
+    else:
+        return redirect("http://isup.me",code=302)
 
 if __name__ == '__main__':
    app.run()

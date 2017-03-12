5d31f99a15a006c06528c8c58cc7904ca9d8e570
shubham <shubham0075@gmail.com>
2017-03-12 18:28:53 +0530

    updated init and form

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
-
 from flask import Flask, render_template, flash, redirect
-from forms import UrlForm
+from forms import UrlForm,ChangeUrlForm
 import forms as f
 from random import randint
 import db_crud
+import bijective
 
 app = Flask(__name__)
 
@@ -16,11 +16,11 @@ def home_page():
 @app.route('/shorten/', methods=['GET', 'POST'])
 def shortener():
     form = UrlForm()
-
+    bijective_obj = bijective.ShortURL()
     if form.validate_on_submit():
-        change_key = "~ck"+str(randint(1000000000000,9999999999999))
+        change_key = "~ck"+str(bijective_obj.encode(randint(1000000000000,9999999999999)))
         if form.custom_alias.data == '':
-            form.custom_alias.data = str(randint(1000000000000,9999999999999))
+            form.custom_alias.data = str(bijective_obj.encode(randint(1000000000000,9999999999999)))
         if db_crud.add_url_record( form.custom_alias.data,f.validate_urls( form.long_url.data),change_key) == "inserted":
             flash('Great ! Go checkout sh.ubham.com/%s' %(form.custom_alias.data))    
             return render_template('form.html',form=form,custom_alias=form.custom_alias.data,link_to = "http://sh.ubham.com/"+form.custom_alias.data, short_url_creation = True ,changekey=change_key)
@@ -34,16 +34,19 @@ def shortener():
 def not_found():
     return render_template('notfound.html')
 
-@app.route('/<page>/')
+@app.route('/<page>/', methods=['GET', 'POST'] )
 def redirect_function(page):
     if page[0:3] == "~ck":
-        return "change page"
+        form1 = ChangeUrlForm()
+        if form1.validate_on_submit(): 
+            new_long_url = f.validate_urls(form1.new_long_url.data)
+            if db_crud.update_url_record(page,new_long_url) == "updated":
+                flash('Destination changed to %s '%(new_long_url))
+                return render_template('change_form.html',form1=form1,new_long_url=new_long_url)
+        return render_template('change_form.html',form1=form1)
     else:
         return redirect( db_crud.expand_url(page) ,code=302)
 
 if __name__ == '__main__':
     app.run()
-
-
-
 

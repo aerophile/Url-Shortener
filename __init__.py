6e3c01cb8a4cd4d678249b8e05ed5a2f1a2a1a7e
shubham <shubham0075@gmail.com>
2017-03-12 16:04:36 +0530

    added non alias entries support

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, flash, redirect
 from forms import UrlForm
 import forms as f
@@ -17,22 +18,32 @@ def shortener():
     form = UrlForm()
 
     if form.validate_on_submit():
-        if db_crud.add_url_record( form.custom_alias.data,f.validate_urls( form.long_url.data),"_"+str(randint(1000000,9999999))+"ck") == "inserted":
+        change_key = "~ck"+str(randint(1000000000000,9999999999999))
+        if form.custom_alias.data == '':
+            form.custom_alias.data = str(randint(1000000000000,9999999999999))
+        if db_crud.add_url_record( form.custom_alias.data,f.validate_urls( form.long_url.data),change_key) == "inserted":
             flash('Great ! Go checkout sh.ubham.com/%s' %(form.custom_alias.data))    
-            return render_template('form.html',form=form,custom_alias=form.custom_alias.data)
+            return render_template('form.html',form=form,custom_alias=form.custom_alias.data,link_to = "http://sh.ubham.com/"+form.custom_alias.data, short_url_creation = True ,changekey=change_key)
         else:
             flash('Woops! The custom alias sh.ubham.com/%s  is taken. Try a different one. '%(form.custom_alias.data))    
-            return render_template('form.html',form=form) 
+            return render_template('form.html',form=form, link_to = "http://sh.ubham.com/shorten", short_url_creation = False) 
 
     return render_template('form.html', form=form)
 
-
+@app.route('/None/')
+def not_found():
+    return render_template('notfound.html')
 
 @app.route('/<page>/')
 def redirect_function(page):
-    return redirect( db_crud.expand_url(page) ,code=302)
+    if page[0:3] == "~ck":
+        return "change page"
+    else:
+        return redirect( db_crud.expand_url(page) ,code=302)
 
 if __name__ == '__main__':
     app.run()
+
+
 
 
